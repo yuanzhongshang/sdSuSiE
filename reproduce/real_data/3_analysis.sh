b7c0f0c2ed4250bb132b7ee4af55c93dba8a9454
yuanzhongshang <37822515+yuanzhongshang@users.noreply.github.com>
2025-12-30 20:07:20 +0800

    Update 3_analysis.sh

--- reproduce/real_data/3_analysis.sh
+++ reproduce/real_data/3_analysis.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --time=10-00:00:00
-#SBATCH --job-name=49_irnt
+#SBATCH --job-name=4079_irnt
 #SBATCH --partition=main,mulan
 #SBATCH --mem=100G
-#SBATCH --array=1-2%200
-#SBATCH --output=/net/mulan/disk2/luliuu/project4/realdata/Gtex/GWASsex5e_8/49_irnt/out/out_%a.out
-#SBATCH --error=/net/mulan/disk2/luliuu/project4/realdata/Gtex/GWASsex5e_8/49_irnt/out/out_%a.err
+#SBATCH --array=1-1%200
+#SBATCH --output=/net/mulan/disk2/luliuu/project4/realdata/Gtex/GWASsex5e_8/4079_irnt/out/out_%a.out
+#SBATCH --error=/net/mulan/disk2/luliuu/project4/realdata/Gtex/GWASsex5e_8/4079_irnt/out/out_%a.err
 
 bash
 
@@ -13,6 +13,6 @@ let k=0
 for ((rep=1; rep<=17239; rep++)); do
 let k=${k}+1
 if [ ${k} -eq ${SLURM_ARRAY_TASK_ID} ]; then
-Rscript 3_susie.R ${rep} 49_irnt
+Rscript 3_susie.R ${rep} 4079_irnt
 fi
 done
